e8c39e20683603b0b1410edac1d38cca6a996801
Vasily <Gavrilov>
2020-10-20 20:44:32 +0700

    develop commit

--- src/BE03/module01/lesson01_introduction/Introduction.java
+++ src/BE03/module01/lesson01_introduction/Introduction.java
@@ -1,10 +1,15 @@
 package BE03.module01.lesson01_introduction;
 
+/**
+ * Add commit from origin develop in IJ IDEA
+ * @author Gavrilov Vasily
+ */
 public class Introduction {
     public static void main(String[] args) {
         for (int i=0; i<5; i++) {
             System.out.print("Hello! My name is ...");
         }
+        System.out.println();
         System.out.println();
         for (int i=0; i<5; i++) {
             System.out.println("I want to be a java developer!");
