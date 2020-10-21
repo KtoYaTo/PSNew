5e9597a777db33c8939c62e8c569a841ddfe05f8
Vasily <Gavrilov>
2020-10-21 22:35:29 +0700

    add space in Introduction.java добавил пробел в класс "Вступление"

--- src/BE03/module01/lesson01_introduction/Introduction.java
+++ src/BE03/module01/lesson01_introduction/Introduction.java
@@ -1,12 +1,20 @@
 package BE03.module01.lesson01_introduction;
 
+/**
+ * Add commit from origin develop in IJ IDEA
+ *
+ * @author Gavrilov Vasily
+ */
 public class Introduction {
     public static void main(String[] args) {
-        for (int i=0; i<5; i++) {
+        for (int i = 0; i < 5; i++) {
+            if(i > 0){
+                System.out.print(" ");
+            }
             System.out.print("Hello! My name is ...");
         }
         System.out.println();
-        for (int i=0; i<5; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println("I want to be a java developer!");
         }
 
