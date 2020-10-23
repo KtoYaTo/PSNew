de2cc1283fd33a2cc6bb3f3ccbcb9de728fa80e6
ildgud <rubgavrilov@gmail.com>
2020-10-23 14:06:38 +0700

    ISSUE-1/module01_lesson04_operators add  task 45

--- src/BE03/module01/lesson04_operators/ProgramTask45.java
+++ src/BE03/module01/lesson04_operators/ProgramTask45.java
@@ -17,17 +17,41 @@ package BE03.module01.lesson04_operators;
  */
 public class ProgramTask45 {
     public static void main(String[] args) {
-        // Write down here the answer to the question.
-        boolean bOe = true;
+        // operator NOT (!) reverses result if we have true, then we get false. And true -> false
+        // &, | - acts on the check for each comparison field
+        // &&, || - This is a lightweight comparison. acts on the check by the first comparison field, if no further is needed, then it does not check.
+        boolean bOne = true;
         boolean bTwo = false;
-        System.out.println(bOe && bTwo); // false -
-        System.out.println(bOe || bTwo); // true -
-        System.out.println(!bOe); // false -
-        System.out.println(bOe ^ bTwo); //
+        System.out.println("------true false-----");
+        System.out.println(bOne && bTwo); // false
+        System.out.println(bOne || bTwo); // true
+        System.out.println(!bOne); // false
+        System.out.println(bOne ^ bTwo); // true
 
-        System.out.println(bOe & bTwo); //
-        System.out.println(bOe | bTwo); //
-        System.out.println(!bTwo);
-        System.out.println(bOe ^ bTwo);
+        System.out.println(bOne & bTwo); // false
+        System.out.println(bOne | bTwo); // true
+        System.out.println(!bTwo); // true
+        System.out.println("------true true-----");
+        bTwo = true;
+        System.out.println(bOne && bTwo); // true
+        System.out.println(bOne || bTwo); // true
+        System.out.println(!bOne); // false
+        System.out.println(bOne ^ bTwo); // false
+
+        System.out.println(bOne & bTwo); // true
+        System.out.println(bOne | bTwo); // true
+        System.out.println(!bTwo); // false
+        System.out.println("------Test-----");
+        /*
+         * a. true || false         = true
+         * b. false && true         = false
+         * c. !true || 1 >= 0       = true
+         * d. false && 5            = Error
+         * e. true &&! false        = true
+         * f. true ^! true          = true
+         * i. 1> = 1 || false       = Error must be >=, then we get true
+         * g. true && 5 - 5         = Error with && must be true or false
+         */
+
     }
 }
